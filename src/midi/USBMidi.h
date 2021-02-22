35d09dbe30fd566e6d76fbac677f441ae0e12a39
Phil Schatzmann <phil_schatzmann@yahoo.com>
2021-02-22 08:55:14 +0100

    add cmakelists.txt

--- src/midi/USBMidi.h
+++ src/midi/USBMidi.h
@@ -34,6 +34,7 @@
 #include "tusb.h"
 #include "PicoSemaphore.h"
 #include "PicoTimer.h"
+#include "USBDescriptor.h"
 
 /* A combination of interfaces must have a unique product id, since PC will save device driver after the first plug.
  * Same VID/PID with different interface e.g MSC (first), then CDC (later) will possibly cause system error on PC.
